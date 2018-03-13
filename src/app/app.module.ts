8be2b18751728cb3527b38565fafeb99467de818
ahmetomerv <ahmetomerv@gmail.com>
2018-03-14 02:43:26 +0300

    developed Article Full component & other updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ArticleMainComponent } from './article-main/article-main.component';
+import { ArticleFullComponent } from './article-full/article-full.component';
 
 
 @NgModule({
@@ -13,10 +14,14 @@ import { ArticleMainComponent } from './article-main/article-main.component';
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    ArticleMainComponent
+    ArticleMainComponent,
+    ArticleFullComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'article', component: ArticleFullComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
