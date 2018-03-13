b678d163db3ccc69c8e5ef2685f8f4ec1dd16cbf
ahmetomerv <ahmetomerv@gmail.com>
2018-03-13 05:56:05 +0300

    articles, navbar components & updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ArticleMainComponent } from './article-main/article-main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavbarComponent,
+    ArticleMainComponent
   ],
   imports: [
     BrowserModule
